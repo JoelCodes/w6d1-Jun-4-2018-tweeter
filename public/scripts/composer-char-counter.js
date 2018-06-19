6dea894f2ceae92bc9c8b41397e2027af775c944
CopterDoctor <Jordananders@me.com>
2018-06-19 12:09:16 -0700

    char counter and color updating implemented

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,30 @@
+function getCurrentCharCount(text) {
+  let stringLength = $(text).val().length;
+  return stringLength;
+}
+
+function charCountUpdate(counterToUpdate, textLengthCallBack) {
+  let currentCount = textLengthCallBack;
+  let newCount = 140 - currentCount;
+  $(counterToUpdate).text(newCount);
+}
+
+function charColorUpdate(counterToUpdate, textLengthCallBack) {
+  let currentCount = textLengthCallBack;
+  let newCount = 140 - currentCount;
+  if (newCount < 0) {
+    $(counterToUpdate).css('color', 'red');
+  } else {
+    $(counterToUpdate).css('color', 'black');
+  }
+}
+
+$(document).ready(function () {
+  // keydown and keyup because keypress doesn't register highlight all text and deleting
+  $('.new-tweet textarea').on('keydown keyup', function (e) {
+    charCountUpdate('.new-tweet span', getCurrentCharCount('.new-tweet textarea'));
+    charColorUpdate('.new-tweet span', getCurrentCharCount('.new-tweet textarea'));
+  });
+
+
+});
