f25f7bd76a9b86e4d3284b67475dfb797b9321f4
CopterDoctor <Jordananders@me.com>
2018-06-19 18:56:15 -0700

    dynamic creation of tweets to HTML

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,69 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+function createTweetElement(tweetData) {
+  let dateCreated = new Date(tweetData.created_at);
+  let today = new Date();
+  let diff = new Date(today - dateCreated);
+  let days  = diff/1000/60/60/24;
+  return $.parseHTML('<article class="tweet"><header><img src="' + tweetData.user.avatars.small + '" alt="user avatar" width="50px" height="50px"><h2>' + tweetData.user.name + '</h2><h6>' + tweetData.user.handle + '</h6></header><p>' + tweetData.content.text + '</p><footer><p>' + parseInt(days) + ' days ago</p><i class="fas fa-heart"></i><i class="fas fa-retweet"></i><i class="fas fa-flag"></i></footer></article>');
+}
+
+function renderTweets(tweetData) {
+  for (let i = 0; i < tweetData.length; i++) {
+    var $tweet = createTweetElement(tweetData[i]);
+    $($tweet).insertAfter('.new-tweet');
+  }
+}
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": {
+        "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+        "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+        "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+      },
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": {
+        "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+        "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+        "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+      },
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  },
+  {
+    "user": {
+      "name": "Johann von Goethe",
+      "avatars": {
+        "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+        "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+        "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+      },
+      "handle": "@johann49"
+    },
+    "content": {
+      "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+    },
+    "created_at": 1461113796368
+  }
+];
+
+$(document).ready(function () {
+  renderTweets(data);
+});
