292b52fd93a372dd1ebaa89da545d2fbc430df7e
CopterDoctor <Jordananders@me.com>
2018-06-21 09:56:35 -0700

    changed port to 3000

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
+const PORT          = 3000;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
