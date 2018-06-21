3adf0247a303e913e22abcdc579e252406932ab9
CopterDoctor <Jordananders@me.com>
2018-06-21 14:56:51 -0700

    GET and POST from database working and comfirmed persistance

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,19 +1,16 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
-    // Saves a tweet to `db`
+    // Saves a tweet to `mongodb`
     saveTweet: function (newTweet, callback) {
-      db.tweets.push(newTweet);
+      db.collection("tweets").insertOne(newTweet);
       callback(null, true);
     },
 
-    // Get all tweets in `db`, sorted by newest first
+    // Get all tweets in `mongodb`, sorted by newest first
     getTweets: function (callback) {
       const sortNewestFirst = (a, b) => a.created_at - b.created_at;
       db.collection("tweets").find().sort(sortNewestFirst).toArray((err, tweets) => {
