c51343aa8bd6b08cf478dfdf2bdfba2df38069b6
JoelCodes <amnshinness@yahoo.com>
2018-07-09 15:47:30 -0700

    Add initial AJAX call

--- src/Tweeter.jsx
+++ src/Tweeter.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { get } from 'axios';
 
 import NavBar from './NavBar.jsx';
 import NewTweetForm from './NewTweetForm.jsx';
@@ -8,15 +9,20 @@ export default class Tweeter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tweets: [{
-        _id: '5b43d77b9d0e4153d6933359', user: { name: 'Jane Baglioni', handle: '@Baglioni3', avatars: { small: 'https://vanillicon.com/ff7423e00a99cc9865e6d722f4c636d6_50.png', regular: 'https://vanillicon.com/ff7423e00a99cc9865e6d722f4c636d6.png', large: 'https://vanillicon.com/ff7423e00a99cc9865e6d722f4c636d6_200.png' } }, content: { text: 'Stuff and things.' }, created_at: 1531172731011,
-      }],
+      tweets: [],
     };
   }
 
   componentDidMount() {
-    // Do an ajax call.
+    this.loadTweets();
+  }
 
+  loadTweets() {
+    // Do an ajax call.
+    get('/tweets')
+      .then(({ data }) => {
+        this.setState({ tweets: data });
+      });
   }
 
   render() {
